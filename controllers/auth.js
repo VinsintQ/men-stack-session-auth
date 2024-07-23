df58de90a966561c108bcd6c42a8d3c1a1735c03
Mueen <hejiy2002@gmail.com>
2024-07-23 12:36:10 +0300

    complete user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const User = require("../models/user.js");
-
+const bcrypt = require("bcrypt");
 const router = express.Router();
-
+const dotenv = require("dotenv");
+dotenv.config();
 //------------------render sign up plate-----------------
 router.get("/sign-up", (req, res, next) => {
   res.render("auth/sign-up.ejs");
@@ -25,7 +26,19 @@ router.post("/sign-up", async (req, res, next) => {
     if (password !== confirm) {
       return res.send("password and confirm not match");
     }
-  } catch (error) {}
+
+    const hashedPassword = bcrypt.hashSync(
+      password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
+
+    //Alternative way
+    const payolad = { username, password: hashedPassword };
+    const user = await User.create(payolad);
+    res.send(`Thanks for signing up ${user.username}`);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
